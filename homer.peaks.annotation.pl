b7b3f0f4cda824982c237ac748929a10d2e68e42
leipinji <leipinji@foxmail.com>
2018-08-26 23:04:08 -0800

    upgrade homer.peaks.annotation.pl

--- homer.peaks.annotation.pl
+++ homer.peaks.annotation.pl
@@ -33,6 +33,15 @@ open my $element_fh, ">",$genome_element or die "can not open genome element fil
 my ($promoter_tss,$exon,$intron,$utr3,$utr5,$intergenic,$tts) = (0,0,0,0,0,0,0);
 
 # output refgene with peaks in promoter region
+open my $UTR3_fh, ">","3-UTR" or die;
+open my $UTR5_fh, ">","5-UTR" or die;
+open my $intergenic_fh, ">","intergenic" or die;
+open my $intron_fh, ">","intron" or die;
+open my $exon_fh, ">", "exon" or die;
+open my $promoter_tss_fh, ">","promoter_tss" or die;
+open my $tts_fh, ">", "tts" or die;
+
+
 while (<$peaks_annotate_fh>) {
         chomp;
         next if /^PeakID/;
@@ -52,15 +61,43 @@ while (<$peaks_annotate_fh>) {
 
         if (($element =~ /^promoter-TSS/) or ($distance >= -1000 and $distance <= 1000)) {
             $promoter_tss += 1;
+			print $promoter_tss_fh $_,"\n";
         }
         else {
-            if ($element =~ /^3'\sUTR/) {$utr3 += 1;}
-            if ($element =~ /^5'\sUTR/) {$utr5 += 1;}
-            if ($element =~ /^Intergenic/) {$intergenic += 1;}
-            if ($element =~ /^intron/) {$intron += 1;}
-            if ($element =~ /^exon/) {$exon += 1;}
-            if ($element =~ /^non-coding.*exon/) {$exon +=1;}
-            if ($element =~ /^TTS/) {$tts += 1;}
+            if ($element =~ /^3'\sUTR/) {
+				$utr3 += 1;
+				print $UTR3_fh $_, "\n";
+				}
+
+            if ($element =~ /^5'\sUTR/) {
+				$utr5 += 1;
+				print $UTR5_fh $_,"\n";
+				}
+
+            if ($element =~ /^Intergenic/) {
+				$intergenic += 1;
+				print $intergenic_fh $_,"\n";
+				}
+
+            if ($element =~ /^intron/) {
+				$intron += 1;
+				print $intron_fh $_,"\n";
+				}
+
+            if ($element =~ /^exon/) {
+				$exon += 1;
+				print $exon_fh $_,"\n";
+				}
+				
+            if ($element =~ /^non-coding.*exon/) {
+				$exon +=1;
+				print $exon_fh $_,"\n";
+				}
+
+            if ($element =~ /^TTS/) {
+				$tts += 1;
+				print $tts_fh $_,"\n";
+				}
         }
         }
 
@@ -75,4 +112,12 @@ Intergenic  $intergenic
 EOF
 print $element_fh $element_file;
 
+system("mkdir homer.anno");
+system("mv 3-UTR homer.anno/");
+system("mv 5-UTR homer.anno/");
+system("mv intergenic homer.anno/");
+system("mv intron homer.anno/");
+system("mv exon homer.anno/");
+system("mv promoter_tss homer.anno/");
+system("mv tts homer.anno/");
 
